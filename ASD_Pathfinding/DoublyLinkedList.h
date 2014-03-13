63062ee73ea4a4eb6cacb23f2f69729853816568
Chris Buckingham <SUPERgluedCHAIN@gmail.com>
2014-03-13 10:16:31 +0000

    Added DoublyLinkedList.h

---
+++ ASD_Pathfinding/DoublyLinkedList.h
@@ -0,0 +1,17 @@
+template <class T>
+
+class doublyLinkedList
+{
+public:
+	//Node creation for the double linked list
+	struct node{
+		T data;
+		//Pointer to the address of the previous node
+		node *prev;
+		//Pointer to the address of the next node
+		node *next;
+	};
+
+private:
+
+};
