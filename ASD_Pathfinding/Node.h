cbab7e12b79548e51447621ada55a20c728bab1c
Chris Buckingham <SUPERgluedCHAIN@gmail.com>
2014-03-13 10:24:51 +0000

    Adjustments

---
+++ ASD_Pathfinding/Node.h
@@ -0,0 +1,38 @@
+template <class T>
+
+class Node
+{
+public:
+
+	//Node creation for the double linked list
+	T data;
+
+	//Pointer to the address of the previous node
+	Node *prev;
+	//Pointer to the address of the next node
+	Node *next;
+
+	//Pointer to the address of the first node
+	Node *first;
+	//Pointer to the address of the last node
+	Node *last;
+
+	//Default constructor
+	Node();
+
+	//Destructor
+	~Node();
+
+private:
+
+};
+
+template <class T>
+Node<T>::Node()
+{
+};
+
+template <class T>
+Node<T>::~Node()
+{
+};
